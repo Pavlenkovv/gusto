3ae8f4ada69196a99398b9174efd4ba8b2352ea9
cpython-projects <74833506+cpython-projects@users.noreply.github.com>
2021-01-13 15:32:00 +0200

    Add files via upload

--- gusto/settings.py
+++ gusto/settings.py
@@ -28,12 +28,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '902w**419b!-v#tk4!^nef=y)*()e^4w7x(96zt#)1+aihu7n2'
-# SECRET_KEY = SECRET_KEY
+# SECRET_KEY = '902w**419b!-v#tk4!^nef=y)*()e^4w7x(96zt#)1+aihu7n2'
+SECRET_KEY = SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://gusto-12-01-2021.herokuapp.com/']
 
 
 # Application definition
